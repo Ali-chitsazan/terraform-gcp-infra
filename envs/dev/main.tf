6af05622ec7f6a2bd97bab0c09404201143ed89b
Ali Chitsazan <ali.chitsazan@torontomu.ca>
2026-04-16 18:04:37 -0700

    Add Argo Module

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -18,3 +18,19 @@ module "nodepool" {
   source       = "../../modules/nodepool"
   cluster_name = module.gke.cluster_name
 }
+
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${module.gke.cluster_endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.cluster_ca_certificate)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://${module.gke.cluster_endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(module.gke.cluster_ca_certificate)
+  }
+}
---
+++ modules/argocd/main.tf
@@ -0,0 +1,17 @@
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = "argocd"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  namespace  = kubernetes_namespace.argocd.metadata[0].name
+
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+
+  values = [
+    file("${path.module}/values.yaml")
+  ]
+}
